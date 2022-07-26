d44f95ad3ed44c553c251a9ef62cd6ab7c30bfb4
JadenYuk05 <109831927+JadenYuk05@users.noreply.github.com>
2022-07-26 14:44:09 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,7 +120,7 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight= current.offsetHeight
-        const secionTop= current.offsetTop - 50;
+        const sectionTop= current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
